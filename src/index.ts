7a0f5302c19e464c7c872ef8ab5161b62e13657f
Josef Jadrny <josef.jadrny@pipedrive.com>
2023-05-04 14:27:13 +0200

    add first scenario

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,27 @@ dotenv.config()
 
 import Fastify from 'fastify'
 import { summaryFromLastEmail } from './scenario/summaryFromLastEmail'
+import emails from "./emails.json"
+
 
 const fastify = Fastify({
   logger: true
 })
 
+export type ScenarioResult = {
+  name: string,
+  result: string,
+}
 
 fastify.get('/', async (request, reply) => {
   const results = await Promise.all([summaryFromLastEmail()])
-  
+
   const html = `<!DOCTYPE html>
   <html>
   <body>
   <h1>ZEN-MODE playground</h1>
+  <h2>Emails</h2>
+  <pre>${JSON.stringify(emails, null, 2)}</pre>
   ${results.map((result) => '<h2>' + result.name + '</h2><p>' + result.result + '</p>')}
   </body>
   </html>`
--- src/scenario/summaryFromLastEmail.ts
+++ src/scenario/summaryFromLastEmail.ts
@@ -1,5 +1,16 @@
+import { ScenarioResult } from ".."
 import { openai } from "../openai"
+import emails from "../emails.json"
 
-export const summaryFromLastEmail = async(): Promise<{name: string, result: string}> => {
-    return {name: summaryFromLastEmail.name, result:Math.random().toString()}
+export const summaryFromLastEmail = async(): Promise<ScenarioResult> => {
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+              {"role": "user", "content": "I need to summarize an email conversation."},
+              {"role": "assistant", "content": "Sure, please provide me with the email conversation."},
+              {"role": "user", "content": emails[0].body},
+          ]
+        })
+
+    return {name: 'Summary from last email', result: completion.data.choices[0].message.content}
 } 
